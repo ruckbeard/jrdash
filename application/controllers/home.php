bed1ef169c5c528836d8f89e3ac04895ec8f1116
ruckbeard <xxxrufioxx@yahoo.com>
2014-07-10 21:32:12 -0400

    added lines of demarcation

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,22 +1,25 @@
 <?php
 
-class Home extends CI_Controller 
-{
+class Home extends CI_Controller {
 
-    public function index() 
-    {
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public function index() {
         $this->load->view('home/inc/header_view');
         $this->load->view('home/home_view');
         $this->load->view('home/inc/footer_view');
     }
 
-    public function register() 
-    {
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public function register() {
         $this->load->view('home/inc/header_view');
         $this->load->view('home/register_view');
         $this->load->view('home/inc/footer_view');
     }
 
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
+    
 //        public function code()
 //        {
 //        hash('sha256', 'custom' . SALT);
