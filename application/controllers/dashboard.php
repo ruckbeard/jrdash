4720d95d0bede051a9edff36fd787b6460191b36
ruckbeard <xxxrufioxx@yahoo.com>
2014-07-10 21:31:29 -0400

    added lines of demarcation

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,25 +1,31 @@
 <?php
 
-class Dashboard extends CI_Controller
-{
-        public function __construct() {
-            parent::__construct();
-            $user_id = $this->session->userdata('user_id');
-            if (!$user_id) {
-                $this->logout();
-            }
-        }
-    
-	public function index()
-	{
-		$this->load->view('dashboard/inc/header_view');
-		$this->load->view('dashboard/dashboard_view');
-		$this->load->view('dashboard/inc/footer_view');
-	}
+class Dashboard extends CI_Controller {
 
-	public function logout()
-	{
-		$this->session->sess_destroy();
-		redirect('/');
-	}
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public function __construct() {
+        parent::__construct();
+        $user_id = $this->session->userdata('user_id');
+        if (!$user_id) {
+            $this->logout();
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public function index() {
+        $this->load->view('dashboard/inc/header_view');
+        $this->load->view('dashboard/dashboard_view');
+        $this->load->view('dashboard/inc/footer_view');
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public function logout() {
+        $this->session->sess_destroy();
+        redirect('/');
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
 }
